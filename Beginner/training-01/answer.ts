fdb597636b66ed4dc547627731867dd8746a2d87
NRI酒井 将大 <m2-sakai@nri.co.jp>
2025-03-18 14:10:59 +0900

    feat: initial commit

---
+++ Beginner/training-01/answer.ts
@@ -0,0 +1,9 @@
+// 1. 名前（string型）と年齢（number型）を入力として受け取り、挨拶を返す関数を作成してください
+function greet(name: string, age: number): string {
+  return `こんにちは、${name}さん！あなたは${age}歳ですね。`;
+}
+
+// 2. 値が `true` であるかを確認する関数 isTrue を作成してください
+function isTrue(value: boolean): boolean {
+  return value === true;
+}
