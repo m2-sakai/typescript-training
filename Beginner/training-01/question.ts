69f641493596097e827fe8a00515635a21c0d78f
NRI酒井 将大 <m2-sakai@nri.co.jp>
2025-03-18 14:16:23 +0900

    feat: add beginner training 02

--- Beginner/training-01/question.ts
+++ Beginner/training-01/question.ts
@@ -1,9 +1,12 @@
 // 1. 名前（string型）と年齢（number型）を入力として受け取り、挨拶を返す関数を作成してください
-function greet(name: string, age: number): string {
+function greet(
+  name /* 型注釈をここに追加 */,
+  age /* 型注釈をここに追加 */
+) /* 戻り値の型注釈 */ {
   return `こんにちは、${name}さん！あなたは${age}歳ですね。`;
 }
 
-// 2. 値が `true` であるかを確認する関数 isTrue を実装してください
-function isTrue(value: boolean): boolean {
+// 2. 値が `true` であるかを確認する関数 isTrue を作成してください
+function isTrue(value /* 型注釈をここに追加 */) /* 戻り値の型注釈 */ {
   return value === true;
 }
